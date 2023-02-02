009d0980df8c34503db116d712d44504f07caf1e
formegusto <formegusto@gmail.com>
2023-02-02 10:39:34 +0900

    위장 : while 문 use

--- 코딩테스트 고득점 Kit/해시/위장.js
+++ 코딩테스트 고득점 Kit/해시/위장.js
@@ -1,12 +1,51 @@
-function solution(clothes) {
+function solution1(clothes) {
   let answer = 1;
   const kinds = {};
 
-  for (let [name, kind] of clothes) {
+  for (let [_, kind] of clothes) {
     if (kinds[kind]) {
       kinds[kind]++;
     } else {
       kinds[kind] = 1;
+    }
+  }
+
+  for (let kind in kinds) {
+    answer *= kinds[kind] + 1;
+  }
+
+  return answer - 1;
+}
+
+function solution2(clothes) {
+  let answer = 1;
+  const kinds = {};
+
+  for (let i = 0; i < clothes.length; i++) {
+    if (kinds[clothes[i][1]]) {
+      kinds[clothes[i][1]]++;
+    } else {
+      kinds[clothes[i][1]] = 1;
+    }
+  }
+
+  for (let kind in kinds) {
+    answer *= kinds[kind] + 1;
+  }
+
+  return answer - 1;
+}
+
+function solution3(clothes) {
+  let answer = 1;
+  const kinds = {};
+
+  let i = 0;
+  while (i < clothes.length) {
+    if (kinds[clothes[i][1]]) {
+      kinds[clothes[i++][1]]++;
+    } else {
+      kinds[clothes[i++][1]] = 1;
     }
   }
 
@@ -28,5 +67,17 @@ const input2 = [
   ["smoky_makeup", "face"],
 ]; // 3
 
-console.log(solution(input1));
-console.log(solution(input2));
+console.time("Solution 1");
+solution1(input1);
+solution1(input2);
+console.timeEnd("Solution 1");
+
+console.time("Solution 2");
+solution2(input1);
+solution2(input2);
+console.timeEnd("Solution 2");
+
+console.time("Solution 3");
+solution3(input1);
+solution3(input2);
+console.timeEnd("Solution 3");
