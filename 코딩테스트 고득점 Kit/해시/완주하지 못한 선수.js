1087be8ea1be21d8e67efdb25322350f5c2f20c6
formegusto <formegusto@gmail.com>
2023-02-02 10:02:32 +0900

    완주하지 못한 선수 : forEach, for of -> legacy for

--- 코딩테스트 고득점 Kit/해시/완주하지 못한 선수.js
+++ 코딩테스트 고득점 Kit/해시/완주하지 못한 선수.js
@@ -22,6 +22,31 @@ function solution(participant, completion) {
   return answer;
 }
 
+function solution2(participant, completion) {
+  let answer = "";
+
+  const completionMemory = {};
+
+  for (let i = 0; i < completion.length; i++) {
+    if (completionMemory[completion[i]]) {
+      completionMemory[completion[i]]++;
+    } else {
+      completionMemory[completion[i]] = 1;
+    }
+  }
+
+  for (let i = 0; i < participant.length; i++) {
+    if (completionMemory[participant[i]]) {
+      completionMemory[participant[i]]--;
+    } else {
+      answer = participant[i];
+      break;
+    }
+  }
+
+  return answer;
+}
+
 const input1 = {
   participant: ["leo", "kiki", "eden"],
   completion: ["eden", "kiki"],
@@ -35,6 +60,14 @@ const input3 = {
   completion: ["stanko", "ana", "mislav"],
 }; // "mislav"
 
-console.log(solution(input1.participant, input1.completion));
-console.log(solution(input2.participant, input2.completion));
-console.log(solution(input3.participant, input3.completion));
+console.time("solution 1");
+solution(input1.participant, input1.completion);
+solution(input2.participant, input2.completion);
+solution(input3.participant, input3.completion);
+console.timeEnd("solution 1");
+
+console.time("solution 2");
+solution2(input1.participant, input1.completion);
+solution2(input2.participant, input2.completion);
+solution2(input3.participant, input3.completion);
+console.timeEnd("solution 2");
