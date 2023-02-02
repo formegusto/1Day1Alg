3951a0b444e16d977037dda1b4678ffde61810b4
formegusto <formegusto@gmail.com>
2023-02-02 10:26:13 +0900

    전화번호 목록 : while 문 테스트

--- 코딩테스트 고득점 Kit/해시/전화번호 목록.py
+++ 코딩테스트 고득점 Kit/해시/전화번호 목록.py
@@ -1,4 +1,6 @@
-def solution(phone_book):
+import time
+
+def solution1(phone_book):
     word_dict = dict()
     for word in phone_book:
         word_dict[word] = 0
@@ -11,10 +13,35 @@ def solution(phone_book):
     
     return True
 
+def solution2(phone_book):
+    i = 0
+    word_dict = dict()
+    while i < len(phone_book):
+        word_dict[phone_book[i]] = 0
+        i += 1
+    
+    i = 0
+    while i < len(phone_book):
+        j = 1
+        while j < len(phone_book[i]):
+            if phone_book[i][:j] in word_dict:
+                return False
+            j += 1
+        i += 1
+    return True
+
+
 input1 = ["119", "97674223", "1195524421"] # False
 input2 = ["123","456","789"] # True
 input3 = ["12","123","1235","567","88"] # False
 
-print(solution(input1))
-print(solution(input2))
-print(solution(input3))
+start = time.time()
+solution1(input1)
+solution1(input2)
+solution1(input3)
+print("solution 1 time : {} second".format(time.time() - start))
+start = time.time()
+solution2(input1)
+solution2(input2)
+solution2(input3)
+print("solution 2 time : {} second".format(time.time() - start))
--- 코딩테스트 고득점 Kit/해시/폰켓몬.js
+++ 코딩테스트 고득점 Kit/해시/폰켓몬.js
@@ -1,4 +1,4 @@
-function solution(nums) {
+function solution1(nums) {
   const selectionMap = {};
   const size = nums.length;
   const maxSelectionSize = size / 2;
@@ -20,10 +20,41 @@ function solution(nums) {
   return answer;
 }
 
+function solution2(nums) {
+  const selectionMap = {};
+  let i = nums.length;
+  const maxSelectionSize = i / 2;
+
+  let answer = 0;
+  while (true) {
+    if (--i < 0) break;
+    if (!selectionMap[nums[i]]) {
+      selectionMap[nums[i]] = true;
+      answer++;
+    } else {
+      continue;
+    }
+
+    if (answer >= maxSelectionSize) {
+      break;
+    }
+  }
+
+  return answer;
+}
+
 const input1 = [3, 1, 2, 3]; // result : 2
 const input2 = [3, 3, 3, 2, 2, 4]; // result : 3
 const input3 = [3, 3, 3, 2, 2, 2]; // result : 2
 
-console.log(solution(input1));
-console.log(solution(input2));
-console.log(solution(input3));
+console.time("Solution 1");
+solution1(input1);
+solution1(input2);
+solution1(input3);
+console.timeEnd("Solution 1");
+
+console.time("Solution 2");
+solution2(input1);
+solution2(input2);
+solution2(input3);
+console.timeEnd("Solution 2");
