9f7dcb6b4ef95b63a8db5c2e663abe447a7eb0dc
formegusto <formegusto@gmail.com>
2023-02-14 10:31:45 +0900

    시간초과이긴한데 이 코드 조금 맘에 듬 ㅎㅎ

--- 코딩테스트 고득점 Kit/탐욕법/조이스틱.js
+++ 코딩테스트 고득점 Kit/탐욕법/조이스틱.js
@@ -1,8 +1,60 @@
-function solution(name) {
-  let initName = "A".repeat(name.length);
+// [ 초깃값, 정답 ]
+/**
+ *
+ * @param {string} init
+ * @param {string} answer
+ * @returns {number}
+ */
+function getMoveCount(init, answer, start, count, direction, depth) {
+  if (init === answer) return 0;
 
-  console.log(initName);
+  while (init[start] === answer[start]) {
+    count++;
+    if (direction === "left") {
+      start--;
+      if (start === -1) start = answer.length - 1;
+    } else if (direction === "right") {
+      start++;
+      if (start === answer.length) start = 0;
+    }
+  }
+
+  init = init.substring(0, start) + answer[start] + init.substring(start + 1);
+
+  const leftStart = start ? start - 1 : answer.length - 1;
+  const rightStart = start === answer.length - 1 ? 0 : start + 1;
+
+  const leftCount = getMoveCount(init, answer, leftStart, 1, "left", depth + 1);
+  const rightCount = getMoveCount(
+    init,
+    answer,
+    rightStart,
+    1,
+    "right",
+    depth + 1
+  );
+
+  console.log(depth);
+
+  return count + Math.min(leftCount, rightCount);
 }
 
-solution("JEROEN");
-solution("JAN");
+function solution(name) {
+  let answer = 0;
+  let initName = "A".repeat(name.length);
+
+  answer += getMoveCount(initName, name, 0, 0, null, 1);
+  for (let i = 0; i < name.length; i++) {
+    if (name[i] !== "A")
+      answer += Math.min(name.charCodeAt(i) - 65, 90 - name.charCodeAt(i) + 1);
+  }
+  return answer;
+}
+
+// solution("JEROEN");
+// solution("JAN");
+
+// solution("JEAAAAN");
+// solution("JEAAANEA");
+
+solution("JEZZEZAAAJEAAAAAAZS");
