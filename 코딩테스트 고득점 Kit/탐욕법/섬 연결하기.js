7678aa213b29c03a165a1470e89ea31c91fc63b4
formegusto <formegusto@gmail.com>
2023-02-15 12:22:59 +0900

    섬 연결했어영,, union-find study 하기!

--- 코딩테스트 고득점 Kit/탐욕법/섬 연결하기.js
+++ 코딩테스트 고득점 Kit/탐욕법/섬 연결하기.js
@@ -1,36 +1,73 @@
+function find(root, node) {
+  if (root[node] === node) return node;
+  return find(root, root[node]);
+}
+
+function union(root, node1, node2) {
+  let p1 = find(root, node1);
+  let p2 = find(root, node2);
+
+  if (p1 < p2) root[p2] = p1;
+  else root[p1] = p2;
+}
+
 function solution(n, costs) {
-  let answer = 0;
-
   costs = costs.sort((a, b) => a[2] - b[2]);
+  console.log(costs);
 
-  let visited = Array(n).fill(false);
-  let visitCount = 0;
-  for (let i = 0; i < costs.length; i++) {
-    const [start, end, cost] = costs[i];
+  const root = Array.from(
+    {
+      length: n,
+    },
+    (_, i) => i
+  );
 
-    if (!visited[start]) {
-      visitCount++;
+  let c = 0;
+  let totalCost = 0;
+  while (c < costs.length) {
+    const [start, end, cost] = costs[c++];
+
+    if (find(root, start) !== find(root, end)) {
+      union(root, start, end);
+      totalCost += cost;
     }
-
-    if (!visited[end]) {
-      visited[end] = true;
-      answer += cost;
-      visitCount++;
-    }
-
-    if (visitCount === n) break;
   }
 
-  return answer;
+  return totalCost;
 }
 
 console.log(
   "answer =>",
   solution(4, [
     [0, 1, 1],
-    // [0, 2, 2],
+    [0, 2, 2],
     [1, 2, 5],
     [1, 3, 1],
     [2, 3, 8],
+  ])
+);
+
+console.log(
+  "\nanswer 2 =>",
+  solution(5, [
+    [0, 1, 5],
+    [1, 2, 3],
+    [2, 3, 3],
+    [3, 1, 2],
+    [3, 0, 4],
+    [2, 4, 6],
+    [4, 0, 7],
+  ])
+);
+
+console.log(
+  "\nanswer 3 =>",
+  solution(5, [
+    [0, 1, 1],
+    [0, 2, 2],
+    [1, 2, 5],
+    [1, 3, 3],
+    [2, 3, 8],
+    [3, 4, 1],
   ])
 );
