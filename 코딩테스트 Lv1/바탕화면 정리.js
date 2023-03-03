78130081f25f1f9b81d51424ea9ba13fdf6b7f5c
formegusto <formegusto@gmail.com>
2023-03-03 10:09:10 +0900

    Lv1 최신 문제 해답

--- 코딩테스트 Lv1/바탕화면 정리.js
+++ 코딩테스트 Lv1/바탕화면 정리.js
@@ -1,5 +1,17 @@
-// 시작 최소 위치, 끝 최소 위치 기록시키면 댐
-function solution(wallpaper) {
-  var answer = [];
-  return answer;
+function solution(wp) {
+  let minR = (minC = Number.MAX_SAFE_INTEGER);
+  let maxR = (maxC = Number.MIN_SAFE_INTEGER);
+
+  for (let i = 0; i < wp.length; i++) {
+    for (let j = 0; j < wp[i].length; j++) {
+      if (wp[i][j] === "#") {
+        minR = Math.min(i, minR);
+        minC = Math.min(j, minC);
+        maxR = Math.max(i, maxR);
+        maxC = Math.max(j, maxC);
+      }
+    }
+  }
+
+  return [minR, minC, ++maxR, ++maxC];
 }
